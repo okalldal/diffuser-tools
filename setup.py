73e250088d7d7dc5f18e7d17278e2b6db47b16fd
okalldal <oscar@kalldal.se>
2022-09-21 17:25:56 +0000

    Initialize package and dev env.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='diffuser_tools',
+    version='0.1.0',
+    description='Tools for working with diffusers',
+    url='https://github.com/okalldal/diffuser-tools',
+    packages=['diffuser_tools'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
